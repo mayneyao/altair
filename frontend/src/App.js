14a43703cffd3cac141636e96d043f683dc38295
mayne <dreama9ain@gmail.com>
2018-04-09 16:24:33 +0800

    [ADD] gif canvas按帧播放

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {withStyles} from 'material-ui/styles';
 import Button from 'material-ui/Button';
+import {CircularProgress} from 'material-ui/Progress';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import TextField from 'material-ui/TextField';
@@ -21,16 +22,27 @@ const styles = theme => ({
         marginRight: theme.spacing.unit,
         width: 200,
     },
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
 });
 
 const pixelMix = (pA, pZ) => {
     return parseInt((pA + pZ) / 2)
 }
 
-const frameMix = (fA, fZ) => {
-    fA.map((a, index) => {
-        return pixelMix(a, fZ[index])
-    })
+const frameMix = (fA, fZ, disposal, transparent_index) => {
+    for (let i = 0; i < fA.length; i += 4) {
+        if (fA[i] == 0 && fA[i + 1] == 0 && fA[i + 2] == 0 && fA[i + 3] == 0) {
+            continue;
+        } else {
+            fZ[i] = fA[i]
+            fZ[i + 1] = fA[i + 1]
+            fZ[i + 2] = fA[i + 2]
+            fZ[i + 3] = fA[i + 3]
+        }
+    }
+    return fZ
 }
 
 class Gif extends React.Component {
@@ -41,7 +53,9 @@ class Gif extends React.Component {
             currentFrame: 0,
             play: true,
             context: null,
-            data: []
+            data: [],
+            speed: 80,
+            uploaded: false
         }
     }
 
@@ -55,10 +69,7 @@ class Gif extends React.Component {
     moveToFrame = (num) => {
         const {context} = this.state
         const {gif, currentFrame, maxFrame, data} = this.state
-
-        // let img = new ImageData(mapAdd(gif[num].data, gif[0].data), gif[num].width, gif[num].height)
-
-        context.putImageData(data[num], 0, 0)
+        context.putImageData(gif[num], 0, 0)
         this.setState({
             currentFrame: num
         })
@@ -74,7 +85,9 @@ class Gif extends React.Component {
             this.setState({
                 currentFrame: this.state.currentFrame + 1,
             })
-            context.putImageData(gif[currentFrame], 0, 0)
+            if (gif[currentFrame]) {
+                context.putImageData(gif[currentFrame], 0, 0)
+            }
         } else if (currentFrame == maxFrame) {
             clearInterval(this.state.intervalId);
             this.setState({
@@ -84,86 +97,74 @@ class Gif extends React.Component {
         }
     }
 
+    handleSpeedChange = (event) => {
+        this.setState({
+            speed: event.target.value,
+            play: false,
+        })
+        clearInterval(this.state.intervalId);
+    }
 
     handleFileChange = (event) => {
         let canvas = document.getElementById("canvas")
-        let img = document.getElementById("img")
         let context = canvas.getContext("2d")
-        // let gl = canvas.getContext('webgl');
-
         const file = event.target.files[0]
-
-        // let buffer = gl.createBuffer(file);
+        this.setState({
+            uploaded: true
+        })
         let fr = new FileReader()
-        // let gif = new GifReader(new Uint8Array(file))
-        // console.log(gif)
+
         fr.onload = () => {
             let gif = new GifReader(new Uint8Array(fr.result))
-
             let allFrames = []
             const frameNums = gif.numFrames()
 
             for (let i = 0; i < frameNums; i++) {
-                let frame = context.createImageData(gif.width, gif.height)
-                gif.decodeAndBlitFrameRGBA(i, frame.data);
-                Object.assign(frame,gif.frameInfo(i))
+                let image = context.createImageData(gif.width, gif.height)
+                gif.decodeAndBlitFrameRGBA(i, image.data);
+                let frame = gif.frameInfo(i)
+                Object.assign(frame, image)
 
                 allFrames.push(frame)
             }
 
-
-            console.log(gif)
-
-            //  disposal = 1 不清除上一帧
-            //  disposal = 2 清除上一帧
-
-            // todo   帧数叠加处理
-
             let parseFrames = [allFrames[0]]
-
+            let images = []
             allFrames.map((eachFrame, index) => {
-                eachFrame.
+                if (index < frameNums && index > 0) {
+                    if (eachFrame.disposal == 1) {
+                        eachFrame.data = frameMix(eachFrame.data, parseFrames[index - 1].data, eachFrame.disposal, eachFrame.transparent_index)
+                    } else if (eachFrame.disposal == 3) {
+                        console.log(index)
+                        eachFrame.data = parseFrames[index - 1].data
+                    }
+                    parseFrames.push(eachFrame)
+                    let image = context.createImageData(gif.width, gif.height)
+                    eachFrame.data.map((i, index) => {
+                        image.data[index] = i
+                    })
+                    images.push(image)
+                    this.setState({
+                        process: (index / frameNums).toFixed() * 100
+                    })
+                }
             })
 
-            gif.decodeAndBlitFrameRGBA(0, image0.data);
-            console.log(gif.frameInfo(0))
-            gif.decodeAndBlitFrameRGBA(1, image1.data);
-
-            context.putImageData(image0, 0, 0)
-            context.putImageData(image1, 0, 0)
+            this.setState({
+                gif: images,
+                maxFrame: frameNums,
+                canvas,
+                context,
+                uploaded: false
+            })
+            this.changeFrame()
         }
         fr.readAsArrayBuffer(file)
-
-
-        // fr.readAsDataURL(file)
-
-        // pixelGif.parse(file).then(gif => {
-        //     console.log(gif)
-        //     let frameData = []
-        //     gif.map(i => {
-        //         let img = new ImageData(i.data, i.width, i.height)
-        //         frameData.push(img)
-        //     })
-        //     // gif.map((i, index) => {
-        //     //     if (mapAdd(data[index] && data[index].data, gif[index + 1] && gif[index + 1].data)) {
-        //     //         let img = new ImageData(mapAdd(data[index] && data[index].data, gif[index + 1] && gif[index + 1].data), gif[index].width, gif[index].height)
-        //     //         data.push(img)
-        //     //     }
-        //     // })
-        //     this.setState({
-        //         gif: frameData,
-        //         maxFrame: gif.length - 1,
-        //         canvas,
-        //         context,
-        //         data: frameData,
-        //     })
-        //     this.changeFrame()
-        // })
     }
 
     componentDidMount() {
         // store intervalId in the state so it can be accessed later:
-        var intervalId = setInterval(this.changeFrame, 60);
+        var intervalId = setInterval(this.changeFrame, this.state.speed);
         this.setState({intervalId: intervalId});
     }
 
@@ -179,7 +180,7 @@ class Gif extends React.Component {
         if (play) {
             clearInterval(this.state.intervalId);
         } else {
-            var intervalId = setInterval(this.changeFrame, 60);
+            var intervalId = setInterval(this.changeFrame, this.state.speed);
             this.setState({intervalId: intervalId});
         }
         this.setState({
@@ -199,12 +200,18 @@ class Gif extends React.Component {
     render() {
         const style = {width: 400, margin: 50};
         const {classes} = this.props;
-        const {currentFrame, maxFrame, gif} = this.state
+        const {currentFrame, maxFrame, gif, uploaded} = this.state
         const bl = (currentFrame / maxFrame * 100).toFixed()
         return (
             <div>
                 <img src="" alt="" id="img"/>
                 <canvas id="canvas"></canvas>
+                <div>
+                    {
+                        uploaded ? <CircularProgress className={classes.progress}/> : <div/>
+                    }
+                </div>
+
                 <div style={style}>
                     <Range
                         defaultValue={[0, 100]} min={0} max={100}
@@ -226,7 +233,6 @@ class Gif extends React.Component {
                 </label>
                 <Button onClick={this.handleStop} variant="raised">暂停/播放</Button>
                 <div>
-                    <span>当前帧:</span>
                     <TextField
                         id="frame"
                         label="frame"
@@ -237,6 +243,18 @@ class Gif extends React.Component {
                         margin="normal"
                     />
                 </div>
+                <div>
+                    <TextField
+                        id="speed"
+                        label="speed"
+                        className={classes.textField}
+                        value={this.state.speed}
+                        onChange={this.handleSpeedChange}
+                        tyoe="number"
+                        margin="normal"
+                    />
+                </div>
+
             </div>
         );
     }
