ba9da0c8afdc91e1078dad2d271cf092932ad1b1
mayne <dreama9ain@gmail.com>
2018-09-19 17:46:40 +0800

    删除我的gif记录

--- src/pages/myGif.js
+++ src/pages/myGif.js
@@ -8,6 +8,8 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import localStorageDB from "localstoragedb";
 
 const styles = theme => ({
@@ -29,24 +31,35 @@ const styles = theme => ({
 class TitlebarGridList extends React.Component {
 	constructor(props) {
 		super(props);
-
-
 		let records = [];
 		// init db
 		let db = new localStorageDB("altair", localStorage);
-
 		if (db.isNew()) {
 			db.createTable("gifx", ["image_url", "caption_template", "create_time"]);
 		} else {
-			records = db.queryAll("gifx");
+			records = db.queryAll("gifx", {sort: [["ID", "DESC"]]});
 		}
-
 		this.state = {
 			tileData: records,
 			db
 		}
 
 	}
+
+	fetchData = () => {
+		const {db} = this.state;
+		let records = db.queryAll("gifx", {sort: [["ID", "DESC"]]});
+		this.setState({
+			tileData: records,
+		})
+	};
+
+	deleteRecord = (id) => {
+		const {db} = this.state;
+		db.deleteRows("gifx", {ID: id});
+		db.commit();
+		this.fetchData();
+	};
 
 	render() {
 		const {classes} = this.props;
@@ -58,7 +71,9 @@ class TitlebarGridList extends React.Component {
 					<div className={classes.root}>
 						<GridList cellHeight={200} className={classes.gridList} cols={2}>
 							<GridListTile key="Subheader" cols={2} style={{height: 'auto'}}>
-								<ListSubheader component="div">我的图库</ListSubheader>
+								<ListSubheader component="div">
+									我的图库
+								</ListSubheader>
 							</GridListTile>
 							{tileData.map(tile => (
 								<GridListTile key={tile.id}>
@@ -67,9 +82,20 @@ class TitlebarGridList extends React.Component {
 										title={tile.title}
 										subtitle={<span/>}
 										actionIcon={
-											<IconButton className={classes.icon} href={`/#/?tmpId=${tile.ID}&from=myGif`}>
-												<InfoIcon/>
-											</IconButton>
+											<div>
+												<IconButton className={classes.icon}
+												            onClick={() => this.deleteRecord(tile.ID)}>
+													<DeleteIcon/>
+												</IconButton>
+												<IconButton className={classes.icon}
+												            href={`/#/?tmpId=${tile.ID}&from=myGif`}>
+													<CloudUploadIcon/>
+												</IconButton>
+												<IconButton className={classes.icon}
+												            href={`/#/?tmpId=${tile.ID}&from=myGif`}>
+													<InfoIcon/>
+												</IconButton>
+											</div>
 										}
 									/>
 								</GridListTile>
