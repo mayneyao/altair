8b819366a91c4379720c50c3049692bd69fd1048
mayne <dreama9ain@gmail.com>
2018-10-23 17:44:19 +0800

    ..

--- src/pkg/wasm_gif.d.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-export function greet(arg0: string): void;
-
-export function rgba2rgb(arg0: Uint8Array): Uint8Array;
-
-export function encode_gif(arg0: number, arg1: number, arg2: Uint8Array): Uint8Array;
-
